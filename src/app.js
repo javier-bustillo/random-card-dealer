b1665366e046f04963330ce9221a43223482dac7
javier-bustillo <jjavier.bustillo@gmail.com>
2022-05-21 18:20:13 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello World!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+};
+let generateRandomNumber = () => {
+  let cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let randomIndexNumber = Math.floor(Math.random() * cardNumber.length);
+
+  return cardNumber[randomIndexNumber];
+};
+let generateRandomSuit = () => {
+  let cardSuit = ["diamond", "spade", "heart", "club"];
+  let randomIndexSuit = Math.floor(Math.random() * cardSuit.length);
+
+  return cardSuit[randomIndexSuit];
 };
